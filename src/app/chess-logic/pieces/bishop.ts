c4799beb93abc16babd54889a9960ba48eff425b
Wilfred Erdo <wilfred05777@users.noreply.github.com>
2024-05-03 07:40:56 +0800

    Bishop Class

---
+++ src/app/chess-logic/pieces/bishop.ts
@@ -0,0 +1,18 @@
+import { FENChar, Coords, Color } from "../models";
+import { Piece } from './piece';
+
+export class Bishop extends Piece{
+  protected override _FENChar: FENChar;
+  protected override _directions: Coords[] = [
+    {x:1, y:1},
+    { x:1, y:-1},
+    { x:-1, y:1},
+    { x:-1, y:-1},
+  ];
+
+  constructor(private piece:Color){
+    super(pieceColor);
+    this._FENChar = pieceColor === Color.White ? FENChar.WhiteBishop : FENChar.BlackBishop;
+  }
+
+}
